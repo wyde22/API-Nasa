f041e2ef2ba7c8e4bdb959ec5eff116d7898a2d5
Dewy Mercerais <dewymercerais@gmail.com>
2018-04-03 14:07:26 +0200

    works on grid of nasa photos of rover

--- assets/js/rover-search.js
+++ assets/js/rover-search.js
@@ -18,16 +18,21 @@ class roverSearch {
         let photosRovers = document.getElementById('photosRovers');
         let dashboard = document.getElementById('dashboard');
 
+        dashboard.className = "pure-u-1";
+        photosRovers.className = "pure-u-1";
+
         let wrapInfos = document.createElement('div');
         let wrapInfosCamera = document.createElement('div');
         let infosRovers = document.createElement('span');
         let infosStatus = document.createElement('span');
         let infosLaunch = document.createElement('span');
         let infosCam = document.createElement('span');
+        let infosLanding = document.createElement('span');
 
         let roverName = result.photos[0].rover.name;
         let roverStatus = result.photos[0].rover.status;
         let roverLaunch = result.photos[0].rover.launch_date;
+        let roverLanding = result.photos[0].rover.landing_date;
         let camName = result.photos[0].camera.full_name;
 
 
@@ -35,15 +40,23 @@ class roverSearch {
         infosStatus.innerHTML = roverStatus;
         infosLaunch.innerHTML = roverLaunch;
         infosCam.innerHTML = camName;
+        infosLanding.innerHTML = roverLanding;
 
         photosRovers.innerHTML = "";
+        dashboard.innerHTML = "";
+
+        wrapInfos.className = "dashboardRover l-box-lrg pure-u-1 pure-u-md-1-2";
+        wrapInfosCamera.className = "dashboardCamera l-box-lrg pure-u-1 pure-u-md-1-2";
 
         dashboard.appendChild(wrapInfos);
         dashboard.appendChild(wrapInfosCamera);
+
         wrapInfosCamera.appendChild(infosCam);
+
         wrapInfos.appendChild(infosRovers);
         wrapInfos.appendChild(infosStatus);
         wrapInfos.appendChild(infosLaunch);
+        wrapInfos.appendChild(infosLanding);
 
         if(photosRovers && result) {
             let datasPhotos = result.photos;
@@ -54,8 +67,9 @@ class roverSearch {
                 console.log(photoRover);
 
                 let div = document.createElement('div');
-				div.className = "item-photos";
+				div.className = "item-photos l-box pure-u-md-1-2 pure-u-lg-1-4";
                 let img = document.createElement('img');
+                img.className = "pure-img";
 
 
 
